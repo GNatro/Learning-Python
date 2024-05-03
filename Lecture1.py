08d17716af59b77a4fac81582a6c03a48fc8c54c
George <george.natroshvili1996@gmail.com>
2024-05-03 23:53:50 +0400

    Overview of how looks Python

---
+++ Lecture1.py
@@ -0,0 +1,13 @@
+x = 0 # This variable stores a value of 0.
+
+print("Learning Indentation") # This is used when we want to print something to the screen.
+print("") # With this, we are just printing a blank line, in other words, a space.
+
+if x == 0:  # This is an if statement.
+    print("In the If Block") # So basically, Python knows that the statement starts when we use indentation. And of course, this is a comment.
+    print("Value of x is 0")
+else: # This is an else statement used after if; it means otherwise do this.
+    print("In the else block.")
+    print("Value of x is non-zero")
+
+print("")
