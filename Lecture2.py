3816b3c03b84ef3abd52ea89e03f71e8ac7c740b
George <george.natroshvili1996@gmail.com>
2024-05-04 00:25:43 +0400

    Learning some stuff about comments

---
+++ Lecture2.py
@@ -0,0 +1,19 @@
+# Define a variable to store a skill
+skill = "DevOps"
+
+""" While we can comment with triple quoutes it is not a recommend practice, to comment
+use the hashtag sign, the triple quotes are used to write large amount of strings """
+
+
+# Multi-line string using triple quotes
+multiline_string = """This is a string of multiple lines.
+It can be used for a huge amount of text."""
+
+# Print statements with explanations
+print(skill)  # This will print the value of the variable 'skill'
+print("skill")  # This will print the string "skill" literally
+
+# Print the multi-line string
+print(multiline_string)
+
+
